a2de75db3e08ca6f51789dd4db17f87159d9f4c4
Maria Cosmen <mcosme00@gmail.com>
2022-12-18 19:49:11 +0900

    Created migration, some seeds and the Task model to display tasks info

--- app.rb
+++ app.rb
@@ -4,11 +4,13 @@ require 'pry-byebug'
 require 'better_errors'
 require 'sinatra/activerecord'
 
-set :database, { adapter: 'sqlite3', database: 'coffee.sqlite3' }
+set :database, { adapter: 'sqlite3', database: 'tasks.sqlite3' }
 # or set :database_file, "path/to/database.yml"
 # El nombre de la DB cuando la creo con rake db:create
 # es el nombre que aparece arriba ("foo.sqlite3")
 
+class Task < ActiveRecord::Base
+end
 
 configure :development do
   use BetterErrors::Middleware
@@ -16,11 +18,15 @@ configure :development do
 end
 
 get '/' do
+  @tasks = Task.all
   erb :tasks
 end
 
-get '/add-task' do
-  erb :add_task
+post '/tasks' do
+  task = Task.new(name: params[:name], description: params[:description], completed: false)
+  task.save
+
+  redirect to('/')
 end
 
 get '/delete-task' do
---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+task = Task.create(name: 'water plants', completed: false)
+task.save
+
+task2 = Task.create(name: 'clean the floor', completed: false)
+task2.save
